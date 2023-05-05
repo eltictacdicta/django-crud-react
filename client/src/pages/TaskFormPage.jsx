5762505bc6d8553b3e2c7265a37192fdb01dcd39
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-05-05 19:44:08 +0200

    Empezamos a trabajar con la lógica del formualario

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,24 @@
+import { useForm } from "react-hook-form";
+
 export function TaskFormPage() {
+
+    const {register, handleSubmit} = useForm()
+
+    const onSubmit = handleSubmit(data=>{
+      console.log(data)
+    })
+    
     return (
       <div>
-          Task Form Page
+          <form onSubmit={onSubmit}>
+            <input type="text" name="title" id="title" placeholder="Titulo"
+            {...register("title", {required:true})}
+            />
+            <textarea rows="3" placeholder="Descripción"
+            {...register("description", {required:true})}
+            ></textarea>
+            <button type="">Guardar</button>
+          </form>
       </div>
     )
   }
